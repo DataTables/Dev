6af8df83ba2273049ebb9577415215ca360923e6
Colin Marks <colin@datatables.net>
2018-07-19 16:14:40 +0100

    tweaks for gemini

--- unit_tests/vagrant.fedora.27/vagrant_test.sh
+++ unit_tests/vagrant.fedora.27/vagrant_test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# Usage: no args runs VM once, or the arg is the sleep interval between continuous iterations
 
 SLEEP=0
 if [ $# -eq 1 ] ; then
 	SLEEP=$1
 fi
+
+# Work out which interface to use (needed on Gemini as it has two)
+export NETWORK_INTERFACE=$(route | grep '^default' | grep -o '[^ ]*$')
+echo "Bridging on network interface $NETWORK_INTERFACE"
 
 while : ; do
 	vagrant destroy -f
