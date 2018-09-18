703ad19658968d5faa3c04d435fe967f30c3b823
Allan Jardine <allan@stargazer>
2018-09-18 13:59:42 +0100

    Postgres database for Editor tests

---
+++ unit_tests/vagrant.fedora.27/setup_postgres.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "Setting up Postgres"
+
+sudo dnf install -y postgresql-server postgresql-contrib
+
+echo "Configuring Postgres"
+
+sudo postgresql-setup --initdb --unit postgresql
+sudo echo "
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+local   all             postgres                                peer
+local   all             all                                     peer
+host    all             all             0.0.0.0/0               md5
+host    all             all             ::1/128                 md5
+" > /var/lib/pgsql/data/pg_hba.conf
+
+sudo echo "listen_addresses = '*'" >> /var/lib/pgsql/data/postgresql.conf
+
+echo "Starting Postgres services"
+
+systemctl start postgresql.service
+systemctl enable postgresql.service
+
+echo "Postgres user and test db"
+
+DBUSER=$(jq -r ".postgres.user" < /vagrant/db.json)
+DBPASS=$(jq -r ".postgres.pass" < /vagrant/db.json)
+DBNAME=$(jq -r ".postgres.db" < /vagrant/db.json)
+
+sudo -u postgres psql -c "CREATE USER \"${DBUSER}\" WITH PASSWORD '${DBPASS}';"
+sudo -u postgres psql -c "CREATE DATABASE \"${DBNAME}\" OWNER \"${DBUSER}\";"
+
+echo "localhost:5432:${DBUSER}:${DBPASS}" > ~/.pgpass
+
+echo "Populating postgres database"
+
+cp /home/vagrant/DataTablesSrc/built/DataTables/extensions/Editor/examples/sql/postgres.sql /tmp
+sudo -d ${DBNAME} -U {$DBUSER} -f /tmp/postgres.sql
+rm /tmp/postgres.sql
+
