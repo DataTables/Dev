8b02ed51c05dc3fc2c2425bbcabdb3ea4c07764b
Colin Marks <colin@datatables.net>
2018-09-21 17:07:54 +0100

    DEV Allan left out the slash again on the cp command (he's doing his best)

--- unit_tests/vagrant.fedora.27/setup_postgres.sh
+++ unit_tests/vagrant.fedora.27/setup_postgres.sh
@@ -36,7 +36,7 @@ echo "localhost:5432:${DBUSER}:${DBPASS}" > ~/.pgpass
 
 echo "Populating postgres database"
 
-cp home/vagrant/DataTablesSrc/built/DataTables/extensions/Editor/examples/sql/postgres.sql /tmp/postgres.sql
+cp /home/vagrant/DataTablesSrc/built/DataTables/extensions/Editor/examples/sql/postgres.sql /tmp/postgres.sql
 chmod +r /tmp/postgres.sql
 sudo -u postgres PGPASSWORD="${DBPASS}" psql -d ${DBNAME} -U {$DBUSER} -f /tmp/postgres.sql
 rm /tmp/postgres.sql
