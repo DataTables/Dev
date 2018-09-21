ec6d200f9fdb1e2af02ac7ad5fb2bfb60e8ecba4
Colin Marks <colin@datatables.net>
2018-09-21 18:49:58 +0100

    DEV fixing more of the intrepid leader's dodgy code

--- unit_tests/vagrant.fedora.27/setup_postgres.sh
+++ unit_tests/vagrant.fedora.27/setup_postgres.sh
@@ -29,6 +29,7 @@ DBUSER=$(jq -r ".postgres.user" < /vagrant/db.json)
 DBPASS=$(jq -r ".postgres.pass" < /vagrant/db.json)
 DBNAME=$(jq -r ".postgres.db" < /vagrant/db.json)
 
+cd /tmp
 sudo -u postgres psql -c "CREATE USER \"${DBUSER}\" WITH PASSWORD '${DBPASS}';"
 sudo -u postgres psql -c "CREATE DATABASE \"${DBNAME}\" OWNER \"${DBUSER}\";"
 
@@ -38,6 +39,7 @@ echo "Populating postgres database"
 
 cp /home/vagrant/DataTablesSrc/built/DataTables/extensions/Editor/examples/sql/postgres.sql /tmp/postgres.sql
 chmod +r /tmp/postgres.sql
-sudo -u postgres PGPASSWORD="${DBPASS}" psql -d ${DBNAME} -U {$DBUSER} -f /tmp/postgres.sql
-rm /tmp/postgres.sql
+sudo -u postgres PGPASSWORD="${DBPASS}" psql -d ${DBNAME} -U ${DBUSER} -f /tmp/postgres.sql
+# TK COLIN rm /tmp/postgres.sql
 
+echo "Completed Postgres configuration"
