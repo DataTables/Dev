d02b96418aac0b2e478dcf242891543c70f69a93
Allan Jardine <allan@stargazer>
2018-10-08 21:27:02 +0100

    Fix - don't delete the SQLite database if it already exists - just run the SQL over it. It will do the drop tables as needed. The problem with removing it was that .NET and NodeJS had connections open to the old file, so it would enter into a readonly mode

--- unit_tests/vagrant.fedora.27/sqlite_create_test_database.sh
+++ unit_tests/vagrant.fedora.27/sqlite_create_test_database.sh
@@ -3,7 +3,8 @@
 SQL=/home/vagrant/DataTablesSrc/built/DataTables/extensions/Editor/examples/sql/sqlite.sql
 DB=/home/vagrant/editor.db
 
-rm -f $DB
+
+#rm -f $DB
 cat $SQL | sqlite3 $DB
 
 sudo chmod 666 /home/vagrant/editor.db
